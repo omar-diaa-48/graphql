de37c36db2f601779eaca6fe31709a190b5447a9
Omar <omar@kamelizer.com>
2021-12-23 02:08:36 +0200

    add db json server

--- schema/schema.js
+++ schema/schema.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const graphql = require('graphql');
 const _ = require('lodash');
 const {
@@ -29,7 +30,8 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: {type: GraphQLString}},
             resolve(parentValue, args){
-                return _.find(users, {id:args.id});
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(res => res.data)
             }
         }
     }
