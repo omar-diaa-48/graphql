235e829c7dad886988548629878f2cf1d073c88d
Omar <omar@kamelizer.com>
2021-12-23 16:39:09 +0200

    user and company circular reference

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 
@@ -14,13 +15,40 @@ const users = [
     {id:'47', firstName:'Samantha', age:21}
 ]
 
+const axiosInstance = axios.create({
+    baseURL:'http://localhost:3000'
+})
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: () => ({
+        id: {type:GraphQLString},
+        name: {type:GraphQLString},
+        description: {type:GraphQLString},
+        users:{
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args){
+                return axiosInstance.get(`/companies/${parentValue.id}/users`)
+                    .then(res => res.data)
+            }
+        }
+    })
+});
+
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields:{
+    fields: () => ({
         id: { type: GraphQLString},
         firstName: { type: GraphQLString},
-        age: { type: GraphQLInt}
-    }
+        age: { type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args){
+                return axiosInstance.get(`/companies/${parentValue.companyId}`)
+                    .then(res => res.data)
+            }
+        }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -30,7 +58,15 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: {type: GraphQLString}},
             resolve(parentValue, args){
-                return axios.get(`http://localhost:3000/users/${args.id}`)
+                return axiosInstance.get(`/users/${args.id}`)
+                    .then(res => res.data)
+            }
+        },
+        company:{
+            type: CompanyType,
+            args: {id: {type:GraphQLString}},
+            resolve(parentValue, args){
+                return axiosInstance.get(`/companies/${args.id}`)
                     .then(res => res.data)
             }
         }
